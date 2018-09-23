194542ad8b528e48b6e2c956bb15c4a775b0d2f5
John Burch <JBurch@acas.org.uk>
2018-09-23 14:50:47 +0000

    none-unique test prod 2

--- web/modules/custom/production/js/test_prod.js
+++ web/modules/custom/production/js/test_prod.js
@@ -6,7 +6,7 @@ var d = new Date();
 Drupal.behaviors.test_prod = {
   attach: function(context, settings) {
     jQuery.ajax({
-      url: "/sync-prod-data?" + d.getTime(),
+      url: "/sync-prod-data",
       type: "GET",
       dataType: "json",
       cache: false,
@@ -50,11 +50,11 @@ function getPage(node) {
     url: prod + node.url,
     type: "GET",
     dataType: "html",
-    cache: false,
+    cache: true,
     node: node,
     timeout: 60000,
     error: function(XMLHttpRequest, textStatus, errorThrown){
-      jQuery("#test-target").html(jQuery("#test-target").html() + '<div class="result"><span class="title">' + node.title + '</span>&nbsp;<span class="red">BAD</span></div>');
+      jQuery("#test-target").html(jQuery("#test-target").html() + '<div class="result"><span class="title">' + node.title + '</span>&nbsp;<span class="red">BAD</span><br />Check the <a href="https://beta.acas.org.uk/admin/reports/dblog">Production log</a> for any PHP SQL deadlock errors (messages beginig with "Drupal\Core\Database\DatabaseExceptionWrapper: SQLSTATE…"). You will have to run the sync again.</div>');
     },
     success: function(data){
       var a1 = data.split('last-changed="');
