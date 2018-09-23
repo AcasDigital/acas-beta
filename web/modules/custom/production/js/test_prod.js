43f81c8d4bb275f662eacc697295461a3db64a11
John Burch <JBurch@acas.org.uk>
2018-09-23 15:11:40 +0000

    none-unique test prod 5

--- web/modules/custom/production/js/test_prod.js
+++ web/modules/custom/production/js/test_prod.js
@@ -52,11 +52,11 @@ function getPage(node) {
     url: prod + node.url,
     type: "GET",
     dataType: "html",
-    cache: true,
+    cache: false,
     node: node,
     timeout: 60000,
     error: function(XMLHttpRequest, textStatus, errorThrown){
-      jQuery("#test-target").html(jQuery("#test-target").html() + '<div class="result"><span class="title">' + node.title + '</span>&nbsp;<span class="red">BAD</span><br />Check the <a href="https://beta.acas.org.uk/admin/reports/dblog">Production log</a> for any PHP SQL deadlock errors (messages beginig with "Drupal\Core\Database\DatabaseExceptionWrapper: SQLSTATE…"). You will have to run the sync again.</div>');
+      jQuery("#test-target").html(jQuery("#test-target").html() + '<div class="result"><span class="title">' + node.title + '</span>&nbsp;<span class="red">BAD</span><br />Check the <a href="https://beta.acas.org.uk/admin/reports/dblog">Production log</a> for any PHP SQL deadlock errors (messages beginning with "Drupal\Core\Database\DatabaseExceptionWrapper: SQLSTATE"). You will have to run the sync again.</div>');
     },
     success: function(data){
       var a1 = data.split('last-changed="');
