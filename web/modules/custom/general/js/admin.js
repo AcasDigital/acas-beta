6c20813638edb38c2b344f312687f884e8100310
John Burch <JBurch@acas.org.uk>
2018-11-30 10:15:57 +0000

    accesabilty

--- web/modules/custom/general/js/admin.js
+++ web/modules/custom/general/js/admin.js
@@ -18,6 +18,13 @@
         var title = $("option:selected", this).text().replace(/-/g, "");
         $("#node-details-page-form #edit-title-0-value").val(title);
       });
+      // Changes to Taxonomy UI
+      $("#toolbar-item-administration-tray .toolbar-menu-administration .toolbar-menu .toolbar-icon-entity-taxonomy-vocabulary-collection").text("Site structure");
+      $(".toolbar-icon-entity-taxonomy-vocabulary-edit-form-acas").parent().parent().hide();
+      $(".toolbar-icon-entity-taxonomy-vocabulary-edit-form-acas").parent().parent().parent().css("background-image", "none");
+      $("#block-seven-primary-local-tasks nav .tabs__tab a:contains('View')").parent().hide();
+      $("#edit-relations summary").text("Place in site structure");
+      $("#edit-relations .form-select").attr("size", "10");
     }
   };
 })(jQuery, Drupal);
