262756b789367087cfb7640f33bf201cc40c882c
John Burch <JBurch@acas.org.uk>
2019-01-28 22:18:22 +0000

    publications_page css

--- web/modules/custom/general/js/publications_page.js
+++ web/modules/custom/general/js/publications_page.js
@@ -2,10 +2,18 @@ Drupal.behaviors.publications_page = {
   attach: function(context, settings) {
     jQuery(context).find('.paragraph--type--document .field--name-field-pages').once('processed').each(function() {
       if (jQuery(this).html() == 1) {
-        jQuery(this).html('&nbsp;' +jQuery( this).text() + '&nbsp;page');
+        jQuery(this).html(',&nbsp;' +jQuery( this).text() + '&nbsp;page');
       }else{
-        jQuery(this).html('&nbsp;' +jQuery(this).text() + '&nbsp;pages');
+        jQuery(this).html(',&nbsp;' +jQuery(this).text() + '&nbsp;pages');
       }
+    });
+    jQuery(context).find('.paragraph--type--document').once('processed').each(function() {
+      var href = jQuery(this).find('.file-link a').attr('href');
+      var ext = href.split('.');
+      ext = ext[ext.length - 1].toUpperCase();
+      jQuery(this).find('.file-link').replaceWith('<span class="file-ext">' + ext + '</span>,');
+      var title = jQuery(this).find('.field--name-field-title').text();
+      jQuery(this).find('.field--name-field-title').replaceWith('<div class="field--name-field-title"><a href="' + href +'">' + title + '</a></div>');
     });
   }
 };
